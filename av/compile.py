c9f138dbc2c2f2d73f4f729ab4800f730d3501c8
sam <github@sampalmer.dev>
2020-04-30 15:20:17 +0100

    Try to compile the files before compiling them all
    There are some yara files which the yara-python library won't accept as
    valid syntax. There's no documentation as to which ones these will be or
    why so I've gone for this brute force approach.
    
    It attempts to compile each file individually, if it works, it allows it
    through to the list which are all compiled together. If it fails, it's
    removed.
    
    It's very inefficient doing everything twice but it works.

--- av/compile.py
+++ av/compile.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python3
 import yara
-
 import os
 
 RULES_DIR = "."
@@ -13,11 +12,17 @@ def get_rules_files():
             if lower_filename.endswith('.yar') or lower_filename.endswith('.yara'):
                 yield os.path.relpath(os.path.join(root, filename), start=RULES_DIR)
 
+def can_file_compile(file_path):
+    try:
+        yara.compile(file_path)
+        return True 
+    except yara.SyntaxError:
+        return False
+
 
 yara_rules = get_rules_files()
 rules_files = {relative_path: os.path.join(RULES_DIR, relative_path)
-               for relative_path in yara_rules}
-
+               for relative_path in yara_rules if can_file_compile(os.path.join(RULES_DIR, relative_path))}
 rules = yara.compile(filepaths=rules_files,
                      externals={'extension': '', 'filename': '', 'filepath': '', 'filetype': ''})
-rules.save("/output")
+rules.save("./output")

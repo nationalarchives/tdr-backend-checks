aabb830f805012400328dc44fe48a2d2c1da58a6
sam <github@sampalmer.dev>
2020-05-05 14:22:51 +0100

    Send the message to the sqs queue for api updates

--- av/src/matcher.py
+++ av/src/matcher.py
@@ -4,6 +4,7 @@ import json
 import logging
 from .version import version
 from datetime import datetime
+import os
 
 FORMAT = '%(asctime)-15s %(message)s'
 INFO = 20
@@ -15,14 +16,15 @@ logger.setLevel(INFO)
 def matcher_lambda_handler(event, lambda_context):
     if "Records" in event:
         outputs = []
+        sqs_client = boto3.client("sqs")
+        s3_client = boto3.client("s3")
         for record in event["Records"]:
             s3 = record["s3"]
             bucket = s3["bucket"]["name"]
             key = s3["object"]["key"]
             logger.info("Object %s found", key)
 
-            s3 = boto3.client("s3")
-            s3_object = s3.get_object(Bucket=bucket, Key=key)
+            s3_object = s3_client.get_object(Bucket=bucket, Key=key)
             streaming_body = s3_object["Body"]
 
             rules = yara.load("output")
@@ -32,8 +34,10 @@ def matcher_lambda_handler(event, lambda_context):
             output = {"software": "yara", "softwareVersion": yara.__version__, "databaseVersion": version,
                       "result": result,
                       "datetime": time}
-            logger.info(json.dumps(output))
             outputs.append(output)
+            logger.info("Key %s processed", key)
+
+            sqs_client.send_message(QueueUrl=os.environ["SQS_URL"], MessageBody=json.dumps(output))
         return outputs
     else:
         logger.info("Message does not contain any records")

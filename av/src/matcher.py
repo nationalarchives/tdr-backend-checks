6feea1b8581f0c0826b022c1d2f8eeb6d6f728f2
sam <github@sampalmer.dev>
2020-05-05 10:08:20 +0100

    Update to use the json from S3
    This is now using the json as it comes in from the S3 event.

--- av/src/matcher.py
+++ av/src/matcher.py
@@ -1,16 +1,40 @@
 import yara
 import boto3
 import json
+import logging
+from .version import version
+from datetime import datetime
+
+FORMAT = '%(asctime)-15s %(message)s'
+INFO = 20
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('matcher')
+logger.setLevel(INFO)
 
 
 def matcher_lambda_handler(event, lambda_context):
-    bucket = event["bucketName"]
-    key = event["key"]
-    rules = yara.load("output")
-    s3 = boto3.client("s3")
-    s3_object = s3.get_object(Bucket=bucket, Key=key)
-    streaming_body = s3_object["Body"]
-    match = rules.match(data=streaming_body.read())
-    result = [x.rule for x in match]
-    return json.dumps({"result": result})
+    if "Records" in event:
+        outputs = []
+        for record in event["Records"]:
+            s3 = record["s3"]
+            bucket = s3["bucket"]["name"]
+            key = s3["object"]["key"]
+            logger.info("Object %s found", key)
 
+            s3 = boto3.client("s3")
+            s3_object = s3.get_object(Bucket=bucket, Key=key)
+            streaming_body = s3_object["Body"]
+
+            rules = yara.load("output")
+            match = rules.match(data=streaming_body.read())
+            result = "\n".join([x.rule for x in match])
+            time = datetime.today().strftime("%Y-%m-%d-%H:%M:%S")
+            output = {"software": "yara", "softwareVersion": yara.__version__, "databaseVersion": version,
+                      "result": result,
+                      "datetime": time}
+            logger.info(json.dumps(output))
+            outputs.append(output)
+        return outputs
+    else:
+        logger.info("Message does not contain any records")
+        return []
